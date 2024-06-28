e17651f0865a3bd83b6584f0065775560a920e8f
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-06-28 06:36:56 +0530

    update restart.js

--- restart.js
+++ restart.js
@@ -1,32 +1,20 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 
-function restartServer() {
-    console.log('Restarting server...');
-    exec('node ' + path.resolve(__dirname, 'index.js'), (err, stdout, stderr) => {
-        if (err) {
-            console.error(`Error restarting server: ${err.message}`);
-            return;
+function startServer() {
+    const server = spawn('node', [path.join(__dirname, 'index.js')], {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    });
+
+    server.on('exit', (code) => {
+        if (code !== 0) {
+            console.log('Server crashed. Restarting...');
+            startServer();
         }
-        console.log(`Server restarted successfully:\n${stdout}`);
     });
 }
 
-// Start the server initially
-restartServer();
-
-// Restart the server if it exits
-process.on('exit', () => {
-    restartServer();
-});
-
-process.on('SIGINT', () => {
-    process.exit();
-});
-
-process.on('SIGTERM', () => {
-    process.exit();
-});
+startServer();
