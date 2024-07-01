5ad0b452cb2cd1727929f9a3003b1f6ff93c27b3
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-01 18:30:58 +0530

    update src/plugin/ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -1,23 +1,14 @@
-import { performance } from 'perf_hooks';
-
 const ping = async (m, sock) => {
   const prefix = /^[\\/!#.]/gi.test(m.body) ? m.body.match(/^[\\/!#.]/gi)[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
 
   if (cmd === "ping") {
-    // Function to measure ping
-const measurePing = async () => {
-  const start = performance.now();
-  await new Promise(resolve => setTimeout(resolve, 100)); // Simulating some async operation
-  const end = performance.now();
-  return Math.round(end - start);
-};
-const ping = await measurePing();
+    const initial = new Date().getTime();
+    await m.react('⚡');
+    const final = new Date().getTime();
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${ping} ms_*`;
+    const text = `*_🔥⃝вσт ѕρєє∂: ${(final - initial)} ms_*`;
     await sock.sendMessage(m.from, { text }, { quoted: m });
-
-    await m.React('⚡');
   }
 }
 
