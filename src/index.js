34a0703a253fbf39c946b5219dd33dac7d45a876
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-01 22:43:33 +0530

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -150,31 +150,16 @@ async function start() {
             Matrix.public = false;
         }
 
-        Matrix.ev.on("connection.update", async update => {
-            const { connection, lastDisconnect } = update;
-            if (connection === "close") {
-                let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-                if (reason === DisconnectReason.connectionClosed) {
-                    console.log(chalk.red("[😩] Connection closed, reconnecting."));
-                    start();
-                } else if (reason === DisconnectReason.connectionLost) {
-                    console.log(chalk.red("[🤕] Connection Lost from Server, reconnecting."));
-                    start();
-                } else if (reason === DisconnectReason.loggedOut) {
-                    console.log(chalk.red("[😭] Device Logged Out, Please Delete Session and Scan Again."));
-                    process.exit();
-                } else if (reason === DisconnectReason.restartRequired) {
-                    console.log(chalk.blue("[♻️] Server Restarting."));
-                    start();
-                } else if (reason === DisconnectReason.timedOut) {
-                    console.log(chalk.red("[⏳] Connection Timed Out, Trying to Reconnect."));
-                    start();
-                } else {
-                    console.error("[🚫️] Something Went Wrong: Failed to Make Connection", reason);
-                }
+        Matrix.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut;
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
+            // reconnect if not logged out
+            if (shouldReconnect) {
+                start();
             }
-
-            if (connection === "open") {
+        } else if (connection === "open") {
                 if (initialConnection) {
                     console.log(chalk.green("😃 Integration Successful️ ✅"));
                     Matrix.sendMessage(Matrix.user.id, { text: `😃 Integration Successful️ ✅` });
@@ -184,6 +169,8 @@ async function start() {
                 }
             }
         });
+
+    Matrix.ev.on('creds.update', saveCreds);
 
         Matrix.ev.on('messages.upsert', async chatUpdate => {
             try {
