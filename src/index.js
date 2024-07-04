3049c84f41bd6d499e9381e7b6da866f8a526cdf
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-04 08:25:05 +0530

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -29,15 +29,9 @@ import config from '../config.cjs';
 import { smsg } from '../lib/myfunc.cjs';
 import pkg from '../lib/autoreact.cjs';
 const { emojis, doReact } = pkg;
-
-const sessionName = "session";
-const app = express();
-const orange = chalk.bold.hex("#FFA500");
-const lime = chalk.bold.hex("#32CD32");
 let useQR;
 let isSessionPutted;
 let initialConnection = true;
-const PORT = process.env.PORT || 3000;
 
 const MAIN_LOGGER = pino({
     timestamp: () => `,"time":"${new Date().toJSON()}"`
@@ -56,115 +50,105 @@ const store = makeInMemoryStore({
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
+const credsPath = path.join(__dirname, '/auth_info_baileys/creds.json');
 
-const sessionDir = path.join(__dirname, 'session');
-const credsPath = path.join(sessionDir, 'creds.json');
-
-if (!fs.existsSync(sessionDir)) {
-    fs.mkdirSync(sessionDir, { recursive: true });
-}
-
-async function downloadSessionData() {
+if (!fs.existsSync(credsPath)) {
     if (!config.SESSION_ID) {
-        console.error('Please add your session to SESSION_ID env !!');
-        process.exit(1);
+        console.log('Please add your session to SESSION_ID env !!');
+        process.exit(1); 
     }
     const sessdata = config.SESSION_ID.split("Ethix-MD&")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
-    try {
-        const response = await axios.get(url);
-        const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
-        await fs.promises.writeFile(credsPath, data);
-        console.log("🔒 Session Successfully Loaded !!");
-        start();
-    } catch (error) {
-        console.error('Failed to download session data:', error);
-        process.exit(1);
-    }
+    console.log(sessdata);
+    axios.get(url)
+        .then(response => {
+            const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
+            return fs.promises.writeFile(credsPath, data);
+        })
+        .then(() => {
+            console.log("🔒 Session Successfully Loaded !!");
+        })
+        .catch(err => {
+            console.error("Failed to fetch session data:", err);
+            process.exit(1);
+        });
 }
 
-if (!fs.existsSync(credsPath)) {
-    downloadSessionData();
-}
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-async function start() {
-    try {
-        const { state, saveCreds } = await useMultiFileAuthState(sessionDir);
-        const { version, isLatest } = await fetchLatestBaileysVersion();
-        console.log(`🤖 Ethix-MD using WA v${version.join('.')}, isLatest: ${isLatest}`);
-        
-        const Matrix = makeWASocket({
-            version,
-            logger: pino({ level: 'silent' }),
-            printQRInTerminal: true,
-            browser: ["Ethix-MD", "safari", "3.3"],
-            auth: state,
-            getMessage: async (key) => {
-                if (store) {
-                    const msg = await store.loadMessage(key.remoteJid, key.id);
-                    return msg.message || undefined;
-                }
-                return { conversation: "Ethix-MD Nonstop Testing" };
+async function connectToWA() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/');
+    const { version, isLatest } = await fetchLatestBaileysVersion();
+    console.log(`🤖 Ethix-MD using WA v${version.join('.')}, isLatest: ${isLatest}`);
+    const Matrix = makeWASocket({
+        version,
+        logger: pino({ level: 'silent' }),
+        printQRInTerminal: true,
+        browser: ["Ethix-MD", "safari", "3.3"],
+        auth: state,
+        getMessage: async (key) => {
+            if (store) {
+                const msg = await store.loadMessage(key.remoteJid, key.id);
+                return msg.message || undefined;
             }
-        });
-        
-        Matrix.serialize = (m) => smsg(Matrix, m, store)
-
-        Matrix.ev.on('connection.update', (update) => {
-            const { connection, lastDisconnect } = update;
-            if (connection === 'close') {
-                if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
-                    start();
-                }
-            } else if (connection === 'open') {
-                if (initialConnection) {
-                    console.log(chalk.green("😃 Integration Successful️ ✅"));
-                    Matrix.sendMessage(Matrix.user.id, { text: `😃 Integration Successful️ ✅` });
-                    initialConnection = false;
-                } else {
-                    console.log(chalk.blue("♻️ Connection reestablished after restart."));
-                }
-            }
-        });
-
-        Matrix.ev.on('creds.update', saveCreds);
-
-        Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
-        Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
-        Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
-
-        if (config.MODE === "public") {
-            Matrix.public = true;
-        } else if (config.MODE === "private") {
-            Matrix.public = false;
+            return {
+                conversation: "Ethix-MD A Powerfull Whatsapp BOT"
+            };
         }
+    });
 
-        Matrix.ev.on('messages.upsert', async (chatUpdate) => {
-            try {
-                const mek = chatUpdate.messages[0];
-                if (!mek.key.fromMe && config.AUTO_REACT) {
-                    console.log(mek);
-                    if (mek.message) {
-                        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-                        await doReact(randomEmoji, mek, Matrix);
-                    }
-                }
-            } catch (err) {
-                console.error('Error during auto reaction:', err);
+    Matrix.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+                connectToWA();
             }
-        });
-    } catch (error) {
-        console.error('Critical Error:', error);
-        process.exit(1);
+        } else if (connection === 'open') {
+            if (initialConnection) {
+                console.log(chalk.green("😃 Integration Successful️ ✅"));
+                Matrix.sendMessage(Matrix.user.id, { text: `😃 Integration Successful️ ✅` });
+                initialConnection = false;
+            } else {
+                console.log(chalk.blue("♻️ Connection reestablished after restart."));
+            }
+        }
+    });
+    
+    Matrix.serialize = (m) => smsg(Matrix, m, store)
+
+    Matrix.ev.on('creds.update', saveCreds);
+
+    Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
+    Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
+    Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
+
+    if (config.MODE === "public") {
+        Matrix.public = true;
+    } else if (config.MODE === "private") {
+        Matrix.public = false;
     }
+
+    Matrix.ev.on('messages.upsert', async (chatUpdate) => {
+        try {
+            const mek = chatUpdate.messages[0];
+            if (!mek.key.fromMe && config.AUTO_REACT) {
+                console.log(mek);
+                if (mek.message) {
+                    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+                    await doReact(randomEmoji, mek, Matrix);
+                }
+            }
+        } catch (err) {
+            console.error('Error during auto reaction:', err);
+        }
+    });
 }
 
-start();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+res.send("*Ethix-MD WhatsApp Bot Working successfully..!*");
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT}`));
+setTimeout(() => {
+connectToWA()
+}, 5000);
