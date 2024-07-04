155d33401041689d3a725899758cbe11a01ef11e
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-04 08:55:31 +0530

    update src/plugin/tquoted.js

--- src/plugin/tquoted.js
+++ src/plugin/tquoted.js
@@ -1,21 +1,19 @@
 const quotedMessage = async (m, gss) => {
-  try {
-    const prefixMatch = m.body.match(/^[\\/!#.]/);
-    const prefix = prefixMatch ? prefixMatch[0] : '/';
-    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+try {
+const prefixMatch = m.body.match(/^[\/!#.]/);
+const prefix = prefixMatch ? prefixMatch[0] : '/';
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).trim().toLowerCase() : '';
+const validCommands = ['q', 'quoted'];
 
-    const validCommands = ['q', 'quoted'];
-
-    if (validCommands.includes(cmd)) {
-      if (!m.quoted) return m.reply('reply to the message!');
-
-      let wokwol = await gss.serialize(await m.getQuotedObj());
-      if (!wokwol.quoted) return m.reply('the replied message does not contain a reply');
-      await wokwol.quoted.copyForward(m.from, true);
-    }
-  } catch (err) {
-    console.error(err);
-  }
+if (validCommands.includes(cmd)) {
+if (!m.quoted) return m.reply('Please reply to a message!');
+const quotedMsg = await m.getQuotedMsg();
+if (!quotedMsg) return m.reply('The replied message does not contain a reply');
+await quotedMsg.copyForward(m.from, true);
+}
+} catch (err) {
+console.error(err);
+}
 };
 
 export default quotedMessage;
