c76053a4bf5dfba90ebef95dfce2557c752112b7
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-13 15:51:51 +0530

    update src/plugin/video.js

--- src/plugin/video.js
+++ src/plugin/video.js
@@ -24,14 +24,14 @@ const video = async (m, Matrix) => {
             document: finalVideoBuffer,
             mimetype: 'video/mp4',
             fileName: `${videoInfo.title}.mp4`,
-            caption: '> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿',
+            caption: `> ${videoInfo.title}\n> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿`,
           };
           await Matrix.sendMessage(m.from, docMessage, { quoted: m });
         } else {
           const videoMessage = {
             video: finalVideoBuffer,
             mimetype: 'video/mp4',
-            caption: '> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿',
+            caption: `> ${videoInfo.title}\n> © POWERED BY 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿`,
           };
           await Matrix.sendMessage(m.from, videoMessage, { quoted: m });
         }
