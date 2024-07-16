a7db3641e9e23194f4ec44cd5cd40dbd322cd49a
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-16 16:19:58 +0530

    update src/plugin/gimage.js

--- src/plugin/gimage.js
+++ src/plugin/gimage.js
@@ -7,35 +7,46 @@ const imageCommand = async (m, sock) => {
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const args = m.body.slice(prefix.length + cmd.length).trim();
-  const query = args;
+  let query = args;
 
   const validCommands = ['image', 'img', 'gimage'];
 
   if (validCommands.includes(cmd)) {
-    if (!query) {
-      return sock.sendMessage(m.from, { text: `Usage: ${prefix + cmd} black cats` });
+    if (!query && !(m.quoted && m.quoted.text)) {
+      return sock.sendMessage(m.from, { text: `Please provide some text, Example usage: ${prefix + cmd} sunnyleone` });
+    }
+    if (!query && m.quoted && m.quoted.text) {
+      query = m.quoted.text;
     }
 
-    try {
-      await m.React("📥");
-      const response = await axios.get(`https://api.guruapi.tech/api/googleimage?text=${encodeURIComponent(query)}`);
+    const match = query.match(/(\d+)/);
+    const numberOfImages = match ? parseInt(match[1]) : 1;
 
-      if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
-        return sock.sendMessage(m.from, { text: 'No images found for your search query.' });
+    try {
+      await sock.sendMessage(m.from, { text: '*Please wait*' });
+
+      const images = [];
+
+      for (let i = 0; i < numberOfImages; i++) {
+        const endpoint = `https://api.guruapi.tech/api/googleimage?text=${encodeURIComponent(query)}`;
+        const response = await axios.get(endpoint, { responseType: 'arraybuffer' });
+
+        if (response.status === 200) {
+          const imageBuffer = Buffer.from(response.data, 'binary');
+          images.push(imageBuffer);
+        } else {
+          throw new Error('Image generation failed');
+        }
       }
 
-      const images = response.data.slice(0, 5); // Get the top 5 images
-
-      for (const imageData of images) {
+      for (let i = 0; i < images.length; i++) {
         await sleep(500);
-        const imageBuffer = Buffer.from(imageData, 'binary');
-
-        await sock.sendMessage(m.from, { image: imageBuffer, caption: '' }, { quoted: m });
+        await sock.sendMessage(m.from, { image: images[i], caption: '' }, { quoted: m });
       }
       await m.React("✅");
     } catch (error) {
       console.error("Error fetching images:", error);
-      await sock.sendMessage(m.from, { text: 'Error fetching images.' });
+      await sock.sendMessage(m.from, { text: '*Oops! Something went wrong while generating images. Please try again later.*' });
     }
   }
 };
