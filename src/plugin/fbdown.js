009c578569e42139faf278cc6d116e8c0613990e
twWld1 <122292274+twWld1@users.noreply.github.com>
2024-08-04 08:51:15 +0530

    Update fbdown.js

--- src/plugin/fbdown.js
+++ src/plugin/fbdown.js
@@ -80,10 +80,10 @@ const facebookCommand = async (m, Matrix) => {
             },
             interactiveMessage: proto.Message.InteractiveMessage.create({
               body: proto.Message.InteractiveMessage.Body.create({
-                text: `*ETHIX-MD FACEBOOK POST DOWNLOADER*\n\n> *TITLE*: ${fbData.title}`
+                text: `*TWORLD-MD FACEBOOK POST DOWNLOADER*\n\n> *TITLE*: ${fbData.title}`
               }),
               footer: proto.Message.InteractiveMessage.Footer.create({
-                text: "© Powered By Ethix-MD"
+                text: "© Powered By TWORLD-MD"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
                 ...(await prepareWAMessageMedia({ image: { url: fbData.thumbnail } }, { upload: Matrix.waUploadToServer })),
@@ -145,7 +145,7 @@ const facebookCommand = async (m, Matrix) => {
             content = { 
               video: finalMediaBuffer, 
               mimetype: 'video/mp4', 
-              caption: '> © Powered by Ethix-MD',
+              caption: '> © Powered by TWORLD-MD',
             };
             await Matrix.sendMessage(m.from, content, { quoted: m });
           } else {
